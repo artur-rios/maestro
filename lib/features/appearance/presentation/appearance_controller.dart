2dd7b3304961e47f9119174aed4a91c3e1d9ff86
Artur Rios <arturdev@duck.com>
2026-08-12 17:05:49 -0300

    feat: control appearance changes

---
+++ lib/features/appearance/presentation/appearance_controller.dart
@@ -0,0 +1,42 @@
+import 'dart:async';
+
+import 'package:flutter/foundation.dart';
+import 'package:maestro/features/appearance/application/appearance_preference_repository.dart';
+import 'package:maestro/features/appearance/domain/appearance_mode.dart';
+
+final class AppearanceController extends ChangeNotifier {
+  AppearanceController({
+    required AppearancePreferenceRepository repository,
+    required AppearanceMode initialMode,
+  }) : _repository = repository,
+       _mode = initialMode;
+
+  final AppearancePreferenceRepository _repository;
+  AppearanceMode _mode;
+  Future<void> _writeTail = Future<void>.value();
+  int _revision = 0;
+
+  AppearanceMode get mode => _mode;
+
+  Future<bool> select(AppearanceMode next) {
+    if (next == _mode) return Future<bool>.value(true);
+    final previous = _mode;
+    final revision = ++_revision;
+    _mode = next;
+    notifyListeners();
+    final completion = Completer<bool>();
+    _writeTail = _writeTail.then((_) async {
+      try {
+        await _repository.save(next);
+        completion.complete(true);
+      } on Object {
+        if (revision == _revision) {
+          _mode = previous;
+          notifyListeners();
+        }
+        completion.complete(revision != _revision);
+      }
+    });
+    return completion.future;
+  }
+}
